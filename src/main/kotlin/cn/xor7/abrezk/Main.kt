0437d841a1843fd4faca5074b213272f7b74c59e
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2023-12-09 20:19:43 +0800

    feat(resourcepack-converter): finish api, suppress some warnings

--- src/main/kotlin/cn/xor7/abrezk/Main.kt
+++ src/main/kotlin/cn/xor7/abrezk/Main.kt
@@ -1,6 +1,5 @@
 package cn.xor7.abrezk
 
-@Suppress("unused")
 fun main(args: Array<String>) {
     // ResourcepackConverter.convert("C:\\Users\\33918\\Desktop\\Abrezk Development\\assets-1.12.2","C:\\Users\\33918\\Desktop\\Abrezk Development\\assets-1.12.2-convert")
     // println(ResourcepackConverter.createCacheDir().absolutePath)
